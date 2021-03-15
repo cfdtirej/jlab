f62d0c7f1873ff66985b7452e9718a0fcd92c336
cfdtirej <53041956+cfdtirej@users.noreply.github.com>
2021-03-16 00:14:35 +0900

    fix

--- jlab-gpu.sh
+++ jlab-gpu.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 docker build -t cfdtirej/jlab:gpu -f Dockerfile.gpu .
+
 docker run --name jlab_notebook_gpu \
            --gpus all \
-	   -v notebook-gpu:/notebook \
+	   -v $PWD/notebook-gpu:/notebook \
 	   -p 8888:8888 \
 	   --restart always \
 	   -d \
