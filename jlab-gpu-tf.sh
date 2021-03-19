e866f1c5c80a5476c99619dd40c324e7a3bfe7e1
cfdtirej <53041956+cfdtirej@users.noreply.github.com>
2021-03-19 16:35:46 +0900

    dir

--- jlab-gpu-tf.sh
+++ jlab-gpu-tf.sh
@@ -4,7 +4,7 @@ docker build -t cfdtirej/jlab:gpu-tf -f Dockerfile.gpu-tf .
 docker run --name jlab_gpu_tf \
            --gpus all \
 	   -v $PWD/notebook-gpu-tf:/notebook \
-	   -p 8889:8888 \
+	   -p 8888:8888 \
 	   --restart always \
 	   -d \
 	   cfdtirej/jlab:gpu-tf
