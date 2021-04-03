61aa56a201899b70dee6fbdd810c6b1f7dfa1639
cfdtirej <53041956+cfdtirej@users.noreply.github.com>
2021-04-03 22:54:48 +0900

    fix

--- jlab-gpu.sh
+++ jlab-gpu.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-docker build -t cfdtirej/jlab:gpu -f Dockerfile.gpu .
+read JUPYTER_TOKEN < passwd.txt
+
+docker build -t cfdtirej/jlab:gpu \
+			 --build-arg JUPYTER_TOKEN=$JUPYTER_TOKEN \
+			 -f Dockerfile.gpu .
 
 docker run --name jlab_notebook_gpu \
            --gpus all \
